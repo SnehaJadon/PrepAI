029d4aa1719d40e56e22297705c087ce21413c0c
Sneha Jadon <93697009+SnehaJadon@users.noreply.github.com>
2025-02-03 17:53:46 +0530

    fix

--- app/page.js
+++ app/page.js
@@ -1,11 +1,20 @@
+"use client"
 import { ClerkProvider } from '@clerk/nextjs' ;
+import {useRouter} from 'next/navigation';
 import {Button}from '@/components/ui/button';
+import { useEffect } from 'react';
 
 export default function Home() {
+
+  const route = useRouter()
+  useEffect(()=>{
+    route.push("/dashboard")
+  },[route])
+  
   return (
     <ClerkProvider>
       <div>
-        <Button>Test</Button>
+        {/* <Button>Test</Button> */}
       </div>
     </ClerkProvider>
   );
